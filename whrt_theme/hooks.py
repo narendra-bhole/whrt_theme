749ffd78df728dc8e66efa5ebcacc1750699a538
Visharad007 <vishu.rns@gmail.com>
2024-11-18 14:41:44 +0530

    edited hooks.py

--- whrt_theme/hooks.py
+++ whrt_theme/hooks.py
@@ -27,7 +27,7 @@ app_license = "mit"
 # include js, css files in header of desk.html
 app_include_css = "/assets/whrt_theme/css/whrt_theme.css"
 app_include_js = "/assets/whrt_theme/js/whrt_theme.js"
-after_install = "whrt_theme.install.apply_website_theme"
+fixtures = ["Website Theme"]
 
 
 # include js, css files in header of web template
